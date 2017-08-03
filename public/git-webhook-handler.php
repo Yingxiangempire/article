14f04bca55ff97e22eb558d9676934a6866737d6
Yingxiangempire <yingxiangempire@outlook.com>
2017-08-03 11:27:57 +0800

    test

--- public/git-webhook-handler.php
+++ public/git-webhook-handler.php
@@ -14,7 +14,7 @@ $content = json_decode($requestBody, true);
 //若是主分支且提交数大于0
 file_put_contents("git-webhook.txt", $content['ref'].'<>'.$content['total_commits_count'], FILE_APPEND);//追加写入
 if ($content['ref']=='refs/heads/master') {
-    file_put_contents("git-webhook.txt", "到这了haha", FILE_APPEND);//追加写入
+    file_put_contents("git-webhook.txt", "你妹到这了haha", FILE_APPEND);//追加写入
     $res = shell_exec("cd {$path} && git reset --hard origin/master && git clean -f && git pull 2>&1 && git checkout master");//以www用户运行
     $res_log = '-------------------------'.PHP_EOL;
     $res_log .= $content['user_name'] . ' 在' . date('Y-m-d H:i:s') . '向' . $content['repository']['name'] . '项目的' . $content['ref'] . '分支push了' . $content['total_commits_count'] . '个commit：' . PHP_EOL;
