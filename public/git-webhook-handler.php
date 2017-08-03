1a7d54353b5ee17e69850b3774069a012ee86b42
Yingxiangempire <yingxiangempire@outlook.com>
2017-08-03 11:11:30 +0800

    test

--- public/git-webhook-handler.php
+++ public/git-webhook-handler.php
@@ -7,16 +7,14 @@
  */
 $path = "/usr/share/nginx/html/article/";
 $requestBody = file_get_contents("php://input");
-file_put_contents("git-webhook.txt", "到这了", FILE_APPEND);//追加写入
 if (empty($requestBody)) {
-    file_put_contents("git-webhook.txt", "失败了hahahahaha", FILE_APPEND);//追加写入
     die('send failssss');
 }
-file_put_contents("git-webhook.txt", $requestBody, FILE_APPEND);//追加写入
 $content = json_decode($requestBody, true);
 //若是主分支且提交数大于0
 if ($content['ref']=='refs/heads/master' && $content['total_commits_count']>0) {
-    $res = shell_exec("cd {$path} && git pull 2>&1");//以www用户运行
+    file_put_contents("git-webhook.txt", "到这了", FILE_APPEND);//追加写入
+    $res = shell_exec("cd {$path} && git reset --hard origin/master && git clean -f && git pull 2>&1 && git checkout master");//以www用户运行
     $res_log = '-------------------------'.PHP_EOL;
     $res_log .= $content['user_name'] . ' 在' . date('Y-m-d H:i:s') . '向' . $content['repository']['name'] . '项目的' . $content['ref'] . '分支push了' . $content['total_commits_count'] . '个commit：' . PHP_EOL;
     $res_log .= $res.PHP_EOL;
